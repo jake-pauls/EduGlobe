06ec61f44a4865f5743df8af2988924423e77213
Alex <alazcano2000@gmail.com>
2021-01-23 19:10:30 -0800

    Added Phong lighting and ambient lighitng, HD Map

--- dist/js/globe.js
+++ dist/js/globe.js
@@ -1,4 +1,5 @@
 //import * as THREE from "three";
+// import { DirectionalLight } from "three";
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(
   90,
@@ -11,20 +12,28 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var texture = new THREE.TextureLoader().load("assets/earthmap.jpg");
+var pointLight = new THREE.PointLight(0xffffff, 5.0, 30);
+pointLight.position.set(10,10,10);
+scene.add(pointLight);
+ 
+var ambient = new THREE.AmbientLight(0xffffff, 0.1);
+scene.add(ambient);
+
+
+var texture = new THREE.TextureLoader().load("assets/worldMapHD.jpg");
 texture.wrapS = THREE.RepeatWrapping;
 texture.wrapT = THREE.RepeatWrapping;
 texture.repeat.set(1,1);
 
 controls = new THREE.OrbitControls(camera, renderer.domElement);
-var geometry = new THREE.SphereGeometry(5,32,32);
-var material = new THREE.MeshBasicMaterial({
+var geometry = new THREE.SphereGeometry(2,32,32);
+var material = new THREE.MeshPhongMaterial({
   map:texture,
 });
 var globe = new THREE.Mesh(geometry, material);
 scene.add(globe);
 
-camera.position.z = 10;
+camera.position.z = 5;
 
 var animate = function(controls){
   requestAnimationFrame(animate);
