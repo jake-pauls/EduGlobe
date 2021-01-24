84ad2430ead898829217d226cd78c0fdb99a4217
jacob-pauls <jakepauls99@gmail.com>
2021-01-23 20:04:37 -0800

    Cleaned up code references and 2D array formatting, temporarily removed disaster api url

--- dist/js/fetch.js
+++ dist/js/fetch.js
@@ -3,13 +3,10 @@
 // COVID Data Per Country
 const COVID_API_URL = "https://disease.sh/v3/covid-19/countries?yesterday=true&twoDaysAgo=false&sort=cases&allowNull=true";
 
-// NASA Events API geoJSON
-const DISASTER_API_URL = "https://eonet.sci.gsfc.nasa.gov/api/v3/events/geojson?limit=5";
-
 window.addEventListener("load", (event) => {
     var covid_button = document.getElementById("covid_button");
 
-    // Event Listeners for Buttons
+    // Event Listeners for buttons
     covid_button.addEventListener('click', () => {
         callAPI(COVID_API_URL);
     });
@@ -33,12 +30,14 @@ function callAPI(api_url) {
         });
 }
 
+// [country, lat, long]
 function processCovidJSON(jsonData) {
     var covidData = new Array();
     for (var i = 0; i < jsonData.length; i++) {
-        countryInfo = jsonData[i].country;
-        latInfo = jsonData[i].countryInfo.lat;
-        longInfo = jsonData[i].countryInfo.long;
-        covidData[i] = [countryInfo, latInfo, longInfo];
+        country = jsonData[i].country;
+        lat = jsonData[i].countryInfo.lat;
+        long = jsonData[i].countryInfo.long;
+        covidData[i] = [country, lat, long];
     }
+    return covidData;
 }
